74062d469d2d3118b6630b864cdd9e84f13e1d0c
Praisg <120684474+Praisg@users.noreply.github.com>
2024-01-13 22:05:02 +0200

    day1.py

---
+++ day1/bandGenerator.py
@@ -0,0 +1,4 @@
+print (" welcome to the Band Name generator.")
+city = input("What is the city that you grew up in?\n")
+pet = input("What is your pet's name?\n")
+print("Your band name could be "+ city+ " " + pet)
---
+++ day1/print.py
@@ -0,0 +1,4 @@
+print("Day 1 - String manipulation")
+print('eg print("Hello World!")')
+
+print("hello "+ input("What is your name?"))
